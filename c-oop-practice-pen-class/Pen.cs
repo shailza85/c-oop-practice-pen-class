2ec981951e435eca26dbe3e4f955cf404358cdf8
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-08-25 22:14:15 -0600

    Added comments.

--- c-oop-practice-pen-class/Pen.cs
+++ c-oop-practice-pen-class/Pen.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace c_oop_practice_pen_class
 {
-    class Pen
+    class Pen //Pen class with Brand, InkColor, InkLevel, MaxInk, HasLid properties
     {
 
         public string Brand { get; set; }
@@ -22,11 +22,13 @@ namespace c_oop_practice_pen_class
         {
             get
             {
-                return Math.Round((_inklevel + 2.5)/5.0)*5.0;     
+                return Math.Round((_inklevel + 2.5)/5.0)*5.0;     //Round inputs to two decimal places
             }
 
             private set
             {
+                //Percentage, must not accept values below 0 or above 100, throw an exception if an attempt to go lower than 0 is made.
+                // and clamp values higher than 100 to 100.
                 if (value > 100)
                 {
                     _inklevel = 100;
@@ -35,7 +37,7 @@ namespace c_oop_practice_pen_class
                 {
                     if (value <= 0)
                     {
-                        throw new Exception("Not enough ink level! Cannot print...");
+                        throw new Exception("Not enough ink level! Cannot print text...");
 
                     }
                     _inklevel = value;
@@ -44,7 +46,7 @@ namespace c_oop_practice_pen_class
             }
         }
 
-        
+        //Constructor
 
         public Pen()
         {
@@ -54,7 +56,7 @@ namespace c_oop_practice_pen_class
             MaxInk = 100;
             HasLid = true;
         }
-
+        //Methods created: lower ink level according to the number of characters written, assuming 10 characters for the version without parameters. One character equals 0.05mL of ink usage.
         public void Write()
         {
             string myString = "Shailza Sharma";
--- c-oop-practice-pen-class/Program.cs
+++ c-oop-practice-pen-class/Program.cs
@@ -6,14 +6,14 @@ namespace c_oop_practice_pen_class
     {
         static void Main(string[] args)
         {
-            Pen myPen = new Pen();
+            Pen myPen = new Pen();  //Object of Pen class. Checking various character counts and check the ink levels.
 
             myPen.Write();
             myPen.Write("ShailzaSharma");
             myPen.Write("HiShailzaSharma");
             myPen.Write("HelloShailzaSharma");
 
-            //Code below will throw the exception: "Not enough ink level! Cannot print..."
+            //Please uncomment the code below to check that will throw the exception: "Not enough ink level! Cannot print..."
             // myPen.Write("C# is a simple & powerful object-oriented programming language developed by Microsoft. C# can be used to create various types of applications, such as web, windows, console applications, or other types of applications using Visual studio.These C# tutorials will help you learn the essentials of C#, from the basic to advance level topics. These tutorials are broken down into sections, where each section contains a number of related topics that are packed with easy to understand explanations, real-world examples, useful tips, informative notes and a points to remember section.");
 
         }
