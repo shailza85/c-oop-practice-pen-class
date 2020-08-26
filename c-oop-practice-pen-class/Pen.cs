4bc60aa9633c7960089c9f1b10417610fd1441f8
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-08-25 22:06:11 -0600

    initial commit

---
+++ c-oop-practice-pen-class/Pen.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c_oop_practice_pen_class
+{
+    class Pen
+    {
+
+        public string Brand { get; set; }
+
+        public string InkColor { get; set; }
+
+       
+        public int MaxInk { get; set; }
+
+        public Boolean HasLid { get; set; }
+
+        private double _inklevel;
+
+        public double InkLevel
+        {
+            get
+            {
+                return Math.Round((_inklevel + 2.5)/5.0)*5.0;     
+            }
+
+            private set
+            {
+                if (value > 100)
+                {
+                    _inklevel = 100;
+                }
+                else
+                {
+                    if (value <= 0)
+                    {
+                        throw new Exception("Not enough ink level! Cannot print...");
+
+                    }
+                    _inklevel = value;
+
+                }
+            }
+        }
+
+        
+
+        public Pen()
+        {
+            Brand = "Pilot";
+            InkColor = "Red";
+            InkLevel = 100;
+            MaxInk = 100;
+            HasLid = true;
+        }
+
+        public void Write()
+        {
+            string myString = "Shailza Sharma";
+            double length = myString.Length;
+            InkLevel -=((length/0.05)/MaxInk);
+            Console.WriteLine("After printing Ink level is:"+InkLevel + "%");
+
+        }
+
+        public void Write(string characterCount)
+        {
+            string myString = characterCount;
+            double lengthOfString = myString.Length;
+            InkLevel -=((lengthOfString/0.05)/MaxInk);
+
+           Console.WriteLine("After printing Ink level is:" + InkLevel+ "%");
+            
+        }
+    }
+}
---
+++ c-oop-practice-pen-class/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace c_oop_practice_pen_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pen myPen = new Pen();
+
+            myPen.Write();
+            myPen.Write("ShailzaSharma");
+            myPen.Write("HiShailzaSharma");
+            myPen.Write("HelloShailzaSharma");
+
+            //Code below will throw the exception: "Not enough ink level! Cannot print..."
+            // myPen.Write("C# is a simple & powerful object-oriented programming language developed by Microsoft. C# can be used to create various types of applications, such as web, windows, console applications, or other types of applications using Visual studio.These C# tutorials will help you learn the essentials of C#, from the basic to advance level topics. These tutorials are broken down into sections, where each section contains a number of related topics that are packed with easy to understand explanations, real-world examples, useful tips, informative notes and a points to remember section.");
+
+        }
+    }
+}
